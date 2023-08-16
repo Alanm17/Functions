694e62a53d25947ccc378c66de0c005f03c8316d
Alanm17 <mukhammadazizkholdarov@gmail.com>
2023-08-16 16:39:28 +0900

    ,.

--- script.js
+++ script.js
@@ -52,16 +52,38 @@
 // console.log(yearUntilRetirement(1991, "Jonas"));
 // console.log(yearUntilRetirement(1980, "bob"));
 
-function cutFruitPieces(fruit) {
-  return fruit * 4;
-}
+// function cutFruitPieces(fruit) {
+//   return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces(apples);
-  const orangePieces = cutFruitPieces(oranges);
-  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
-  return juice;
-}
-console.log(fruitProcessor(2, 3));
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces(apples);
+//   const orangePieces = cutFruitPieces(oranges);
+//   const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+//   return juice;
+// }
+// console.log(fruitProcessor(2, 3));
 
 // console.log(cutFruitPieces);
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(` ${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired :)`);
+    return -1;
+  }
+};
+
+// return ` ${firstName} retires in ${retirment} years`;
+
+console.log(yearUntilRetirement(1991, "Jonas"));
+console.log(yearUntilRetirement(1950, "Mike"));
